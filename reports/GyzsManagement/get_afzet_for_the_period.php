332d28a08975df3cb148f7697719e86a43ef2360
jyoti-peshwani-github <jyoti.xelcomm@gmail.com>
2022-05-13 12:42:17 +0530

    year will day before current year

--- reports/GyzsManagement/get_afzet_for_the_period.php
+++ reports/GyzsManagement/get_afzet_for_the_period.php
@@ -21,7 +21,7 @@ function getAfzet_365($days) {
   if($days == '1 year') {
     $lastyear = strtotime("-1 year", time());
     $last_year_date = date("Y-m-d", $lastyear);
-    $sql_all_orders = "SELECT entity_id, created_at FROM mage_sales_flat_order WHERE state != 'canceled' AND (created_at >= '".$last_year_date." 00:00:00' AND  created_at <= '".$current_date." 00:00:00')";
+    $sql_all_orders = "SELECT entity_id, created_at FROM mage_sales_flat_order WHERE state != 'canceled' AND (created_at >= '".$last_year_date." 00:00:00' AND  created_at < '".$current_date." 00:00:00')";
   } else {
     $lastyear = strtotime("-".$days." day", time());
     $last_year_date = date("Y-m-d", $lastyear);
