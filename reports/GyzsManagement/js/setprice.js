3c5a83069132b98e2294ea9d18566cd8d0a85883
jyoti-peshwani-github <jyoti.xelcomm@gmail.com>
2022-05-24 17:23:24 +0530

    fixed second check-all issue informed by Peter

--- reports/GyzsManagement/js/setprice.js
+++ reports/GyzsManagement/js/setprice.js
@@ -4019,12 +4019,19 @@ $("#flexCheckDefault").change(function () {
     if ($('input.show_deb_cols').is(":checked") && cat_all_str != '' && cat_all_str != -1) {//means this is a group list
       var cat_all_arr = cat_all_str.split(',');
       if (current_status) { // check all hiddencategories
+
         $.each(cat_all_arr, function (key, value) {
-          $("li[data-id='" + value + "']").children('a').children('i').addClass('checked');
+          var $li = $('li[data-id=' + value + ']');
+          checkGiven($li, true, true);
         });
       } else { //uncheck all hiddencategories
-        $.each(cat_all_arr, function (key, value) {
+       /*  $.each(cat_all_arr, function (key, value) {
           $("li[data-id='" + value + "']").children('a').children('i').removeClass('checked');
+        }); */
+
+        $.each(cat_all_arr, function (key, value) {
+          var $li = $('li[data-id=' + value + ']');
+          checkGiven($li, false, true);
         });
       }
     } else if (current_status) {
@@ -4103,77 +4110,4 @@ $("#flexCheckDefault").change(function () {
     }
     return true;
   }
-
-  function checkIt(status, flag)
-  {
-    $("i.sim-tree-checkbox").each(function() {
-      var $check = $(this);
-      var $li = $check.closest('li');
-      var $childUl, $childUlCheck;
-      var data = $li.data();
-      if (typeof status === 'undefined') {
-        status = !data.checked;
-      }
-
-      if (status === true) {
-        $('#flexCheckDefault').prop('checked', true);
-        $check.removeClass('sim-tree-semi').addClass('checked');
-      } else if (status === false) {
-        $('#flexCheckDefault').prop('checked', false);
-        $check.removeClass('checked sim-tree-semi');
-      } else if (status === 'semi') {
-        $check.removeClass('checked').addClass('sim-tree-semi');
-      }
-      $li.data('checked', status);
-   });
-  }
-
-  function setParentCheck(e) {
-    var t,
-    i = e.parent("ul"),
-    s = i.parent("li"),
-    n = i.children("li"),
-    a = s.find(">a .sim-tree-checkbox"),
-    r = [],
-    d = n.length;
-  s.length &&
-    (e.find(">a .sim-tree-checkbox").hasClass("sim-tree-semi")
-      ? doCheck(a, "semi")
-      : ($.each(n, function () {
-          !0 === $(this).data("checked") && r.push($(this));
-        }),
-        (t = r.length),
-        d === t && doCheck(a, !0),
-        t || doCheck(a, !1),
-        t >= 1 && t < d && doCheck(a, "semi")));
-  }
-
-  function  doCheck (e, t, i) {
-    var s = e.closest("li"),
-      n = s.data();
-    void 0 === t && (t = !n.checked),
-      !0 === t ? e.removeClass("sim-tree-semi").addClass("checked") : !1 === t ? e.removeClass("checked sim-tree-semi") : "semi" === t && e.removeClass("checked").addClass("sim-tree-semi"),
-      s.data("checked", t),
-      !0 ===/*  this.options.linkParent && */ !i && setParentCheck(s);
-  }
-
-  function checkGiven($li, status, flag)
-  {
-    var data = $li.data();
-    if (typeof status === 'undefined') {
-        status = !data.checked;
-    }
-    var $a = $li.children('a');
-    var $check = $a.children('.sim-tree-checkbox');
-    if (status === true) {
-      $check.removeClass('sim-tree-semi').addClass('checked');
-    } else if (status === false) {
-      $check.removeClass('checked sim-tree-semi');
-    } else if (status === 'semi') {
-      $check.removeClass('checked').addClass('sim-tree-semi');
-    }
-    $li.data('checked', status);
-    setParentCheck($li);
-  }
-
 });
